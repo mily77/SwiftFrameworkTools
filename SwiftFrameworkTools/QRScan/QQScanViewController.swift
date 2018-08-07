0205f3df8ec3da5bcc436454b15a32d1e6abc96a
mily77 <773258709@qq.com>
2018-08-07 16:13:29 +0800

    add image

--- SwiftFrameworkTools/QRScan/QQScanViewController.swift
+++ SwiftFrameworkTools/QRScan/QQScanViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class QQScanViewController: UIViewController {
 
+    /**
+     @brief  扫码区域上方提示文字
+     */
+    var topTitle:UILabel?
+    
+    /**
+     @brief  闪关灯开启状态
+     */
+    var isOpenedFlash:Bool = false
+    
+    // MARK: - 底部几个功能：开启闪光灯、相册、我的二维码
+    
+    //底部显示的功能项
+    var bottomItemsView:UIView?
+    
+    //相册
+    var btnPhoto:UIButton = UIButton()
+    
+    //闪光灯
+    var btnFlash:UIButton = UIButton()
+    
+    //我的二维码
+    var btnMyQR:UIButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
