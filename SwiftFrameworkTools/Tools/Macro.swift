41b3180f3e1f990b95487fb5d01793cb3c38a5c8
mily77 <773258709@qq.com>
2018-08-10 14:23:29 +0800

    change Macro

--- SwiftFrameworkTools/Tools/Macro.swift
+++ SwiftFrameworkTools/Tools/Macro.swift
@@ -42,3 +42,27 @@ func kRGBAColor(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) -> UIColor {
 func kRGBColor(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
     return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: 1.0)
 }
+
+// MARK: - 时间格式
+enum TimeFormat:String
+{
+    case format_default            = "yyyy-MM-dd HH:mm:ss"
+    case format_yyMdHm             = "yy-MM-dd HH:mm"
+    case format_yyyyMdHm           = "yyyy-MM-dd HH:mm"
+    case format_yMd                = "yyyy-MM-dd"
+    case format_MdHms              = "MM-dd HH:mm:ss"
+    case format_MdHm               = "MM-dd HH:mm"
+    case format_Hms                = "HH:mm:ss"
+    case format_Hm                 = "HH:mm"
+    case format_Md                 = "MM-dd"
+    case format_yyMd               = "yy-MM-dd"
+    case format_YYMMdd             = "yyyyMMdd"
+    case format_yyyyMdHms          = "yyyyMMddHHmmss"
+    case format_yyyyMdHmsS         = "yyyy-MM-dd HH:mm:ss.SSS"
+    case format_yyyyMMddHHmmssSSS  = "yyyyMMddHHmmssSSS"
+    case format_yMdWithSlash       = "yyyy/MM/dd"
+    case format_yM                 = "yyyy-MM"
+    case format_yMdChangeSeparator = "yyyy.MM.dd"
+}
+
+
