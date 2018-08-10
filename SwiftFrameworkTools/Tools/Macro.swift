8e6b83533f4620d30dcfbaa58d8d9ec5a9ed2d80
mily77 <773258709@qq.com>
2018-08-10 11:41:20 +0800

    change Macro

--- SwiftFrameworkTools/Tools/Macro.swift
+++ SwiftFrameworkTools/Tools/Macro.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
+
+// MARK:- 屏幕大小宽高
 //    定义2个常用的常量
 let kScreenHeight = UIScreen.main.bounds.size.height
 let kScreenWidth = UIScreen.main.bounds.size.width
+func kScreenBounds() -> CGRect {
+    return UIScreen.main.bounds
+}
+func kScreenSize() -> CGSize {
+    return kScreenBounds().size
+}
+func kScreenWidth() -> CGFloat {
+    return kScreenSize().width
+}
+func kScreenHeight() -> CGFloat {
+    return kScreenSize().height
+}
 
-
-
-
-
+// MARK:- 颜色方法
+// MARK:- 十六进制
+func kRGBColorFromHex(rgbValue: Int) -> UIColor {
+    return UIColor(red: ((CGFloat)((rgbValue & 0xFF0000) >> 16)) / 255.0,
+                   green: ((CGFloat)((rgbValue & 0xFF00) >> 8)) / 255.0,
+                   blue: ((CGFloat)(rgbValue & 0xFF)) / 255.0,
+                   alpha: 1.0)
+}
+// MARK:- 带透明度
+func kRGBAColor(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) -> UIColor {
+    return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: a)
+}
+// MARK:- 不带透明度
+func kRGBColor(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+    return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: 1.0)
+}
